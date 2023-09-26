25d6fd853d449486b2217471c1ea1e4a4c7aae18
bgjedlik <bolya.gabor@jedlik.eu>
2023-09-26 11:03:36 +0200

    StringToIntValidationRule

---
+++ 06_wpf_validalas/StringToIntValidationRule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace _06_wpf_validalas
+{
+    public class StringToIntValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+           if (int.TryParse(value.ToString() , out int i))
+            {
+                return new ValidationResult(true, null);
+            }
+            return new ValidationResult(false, "Csak szám lehet!");
+        }
+    }
+}
